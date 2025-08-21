a27396fc4f14557d0dfef93ad693a73a1ccb0ceb
JThilinaDK123 <JThilinaDK123@gmail.com>
2025-08-21 09:47:42 +0530

    Initial MLOps Titanic app updated newwww

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder 
 
 DATA_PATH = Path("data/titanic.csv")
 ARTIFACT_DIR = Path("artifacts")
